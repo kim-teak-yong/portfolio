27d7eaa1a3eb1ce709e5cfa26eb2da86d025a139
kim-teak-yong <126781341+kim-teak-yong@users.noreply.github.com>
2023-03-04 00:58:58 +0900

    first commit

--- js/custom.js
+++ js/custom.js
@@ -1,24 +1,51 @@
-$(document).ready(function(){
-    $(".1dp").on('click', function(){
+    $(".1dp").on('mouseenter mouseleave', function () {
         $(this).find(".Depth2").toggleClass("on"); // li 에서 Dpeth2 찾아 toggle 클래스
         $(this).siblings().children(".Depth2").removeClass("on"); // 선택자 제외한 형제 요소에서 Dpeth2 찾아 remove 클래스
+        $('.blur').toggleClass('on');
     });
-});
 
-// $('.gnb > li').click(function(){
-//     if($('.Depth2').hasClass('on')){
-//         $(this).removeClass('on');
-//         $(".header_bg").removeClass('active');
-//     }else{
-//         $(this).addClass('on');
-//         $(".header_bg").addClass('active');
-//     }
-// });
+    // $(".listbtn").click(function(){
+    //     $('.full_meun').fadeIn();
+    // });
+
+    // $(".close").click(function(){
+    //     $('.full_meun').fadeOut();
+    // });
 
 
-const changeHandler = () => {
-    console.log(window.innerHeight);
-    document.querySelector("video").style.height=String(window.innerHeight)+"px";
-  };
-  
-  window.addEventListener("resize", changeHandler);
+    //PC full menu open / close
+    $(function () {
+
+        const $open = $(".listbtn");
+        const $close = $(".close");
+
+        $($open).click(function () {
+            $(this).toggleClass('on');
+            $('.full_meun').toggleClass('on');
+        });
+
+        $($close).click(function () {
+            $(this).removeClass('on');
+            $('.full_meun').removeClass('on');
+            $($open).toggleClass('on')
+        });
+
+    });
+
+    // $('.gnb > li').on(function(){
+    //     if($('.Depth2').hasClass('on')){
+    //         $(this).removeClass('on');
+    //         $(".header_bg").removeClass('active');
+    //     }else{
+    //         $(this).addClass('on');
+    //         $(".header_bg").addClass('active');
+    //     }
+    // });
+
+    //main video width + height 비율조절
+    const changeHandler = () => {
+        console.log(window.innerHeight);
+        document.querySelector("video").style.height = String(window.innerHeight) + "px";
+    };
+
+    window.addEventListener("resize", changeHandler);
