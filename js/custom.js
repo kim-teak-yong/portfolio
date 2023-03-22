e294bc1e87bb990c1eb12c049dd010b87cda447b
kim-teak-yong <126781341+kim-teak-yong@users.noreply.github.com>
2023-03-22 13:43:26 +0900

    mobile

--- js/custom.js
+++ js/custom.js
@@ -42,18 +42,11 @@ $(function () {
 //     }
 // });
 
-function isMobile(){
-	var UserAgent = navigator.userAgent;
+var UserAgent = navigator.userAgent;
 
-	if (UserAgent.match(/iPhone|iPod|Android|Windows CE|BlackBerry|Symbian|Windows Phone|webOS|Opera Mini|Opera Mobi|POLARIS|IEMobile|lgtelecom|nokia|SonyEricsson/i) != null || UserAgent.match(/LG|SAMSUNG|Samsung/) != null){
-		return true;
-	}else{
-		return false;
-	}
+if (UserAgent.match(/iPhone|iPod|Android|Windows CE|BlackBerry|Symbian|Windows Phone|webOS|Opera Mini|Opera Mobi|POLARIS|IEMobile|lgtelecom|nokia|SonyEricsson/i) != null || UserAgent.match(/LG|SAMSUNG|Samsung/) != null) {
+    location.href = "../mobile/index.html";
 }
-
-if(isMobile()){
-	location.href = "../mobile/index.html";	//모바일페이지
-}else{
-	location.href = "index.html";	//PC페이지
+else {
+    
 }
