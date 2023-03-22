4abb53c9ff48d92e6b6bdd06eadd65348c9bff19
kim-teak-yong <126781341+kim-teak-yong@users.noreply.github.com>
2023-03-22 11:50:27 +0900

    moblie

--- js/custom.js
+++ js/custom.js
@@ -45,7 +45,7 @@ $(function () {
 var UserAgent = navigator.userAgent;
 
 if (UserAgent.match(/iPhone|iPod|Android|Windows CE|BlackBerry|Symbian|Windows Phone|webOS|Opera Mini|Opera Mobi|POLARIS|IEMobile|lgtelecom|nokia|SonyEricsson/i) != null || UserAgent.match(/LG|SAMSUNG|Samsung/) != null) {
-    location.href = "/moblie/index.html";
+    location.href = "/mobile/index.html";
 }
 else {
 
