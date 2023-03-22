99adc4211717a542e88c46c26eb378e16e6fc622
kim-teak-yong <126781341+kim-teak-yong@users.noreply.github.com>
2023-03-22 18:40:36 +0900

    mobile

--- js/custom.js
+++ js/custom.js
@@ -41,12 +41,3 @@ $(function () {
 //         $(".header_bg").addClass('active');
 //     }
 // });
-
-var UserAgent = navigator.userAgent;
-
-if (UserAgent.match(/iPhone|iPod|Android|Windows CE|BlackBerry|Symbian|Windows Phone|webOS|Opera Mini|Opera Mobi|POLARIS|IEMobile|lgtelecom|nokia|SonyEricsson/i) != null || UserAgent.match(/LG|SAMSUNG|Samsung/) != null) {
-    location.href = "https://kim-teak-yong.github.io/portfolio/mobile/index.html";
-}
-else {
-    
-}
